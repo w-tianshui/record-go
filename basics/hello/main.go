2c4ad37aaacd8ecbcacbb21c1dad592704c5616a
w-tianshui <w-tianshui@outlook.com>
2026-03-25 23:29:01 +0800

    update

--- basics/hello/main.go
+++ basics/hello/main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+func intSeq() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
 func main() {
+	nextInt := intSeq()    // 定义nextInt为一个函数，nextInt在被调用的时候会让自由变量/捕获变量i++，并输出
+	fmt.Println(nextInt()) // i = 1
+	fmt.Println(nextInt()) // i = 2
+	fmt.Println(nextInt()) // i = 3
 	fmt.Println("hello world")
 }
